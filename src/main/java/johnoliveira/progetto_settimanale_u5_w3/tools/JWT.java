85a86d4457c090a4449517e706c59abd6a6c0b58
John-9813 <john.oliver98.br@gmail.com>
2024-11-08 15:42:20 +0100

    JWT completato

--- src/main/java/johnoliveira/progetto_settimanale_u5_w3/tools/JWT.java
+++ src/main/java/johnoliveira/progetto_settimanale_u5_w3/tools/JWT.java
@@ -1,17 +1,54 @@
 package johnoliveira.progetto_settimanale_u5_w3.tools;
 
+import io.jsonwebtoken.security.Keys;
+import johnoliveira.progetto_settimanale_u5_w3.exceptions.UnauthorizedException;
 import org.springframework.stereotype.Component;
+
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+
+
+import java.util.Date;
 
 @Component
 public class JWT {
 
-    // Metodo per verificare la validità del token (temporaneo=
-    public void verifyToken(String token) {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    // genera token per un singolo user
+    public String generateToken(String userId) {
+
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .subject(String.valueOf(userId))
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .compact();
     }
 
-    // Metodo per estrarre l'ID utente dal token (temporaneo)
-    public String getIdFromToken(String token) {
-        return "decoded-user-id";
+    // verifica tokem
+    public void verifyToken(String accessToken) {
+        try {
+            Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                    .build().parse(accessToken);
+            // lancerà diversi tipi di eccezioni in base a diverse possibili variazioni del token
+
+        } catch (Exception ex) {
+            throw new UnauthorizedException("Problemi con il token! Per favore effettua di nuovo il login!");
+        }
+    }
+
+    // ottengo lo userId dal token
+    public String getIdFromToken(String accessToken) {
+        return Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .build()
+                .parseSignedClaims(accessToken)
+                .getPayload().getSubject();
     }
 }
+
 
